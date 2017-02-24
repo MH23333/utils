01b8c5803c483c18c9d8eb5129a7941a9a234a59
MatthewHu <seeyalove@163.com>
2017-02-24 15:16:09 +0800

    Signed-off-by: MatthewHu <seeyalove@163.com> add get_datetime method

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import datetime
+
 
 def readfile(filename, separator='\t'):
     res = []
@@ -6,3 +8,7 @@ def readfile(filename, separator='\t'):
             arr = f.split(separator)
             res.append(arr)
     return res
+
+
+def get_datetime(date_format='%Y/%m/%d %H:%M:%S'):
+    return datetime.datetime.now().strftime(format=date_format)
